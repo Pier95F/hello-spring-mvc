c3f92b03f63ddc73d1f92a5cda1aeb4f05b129fa
Pier95F <forcinapierpaolo@gmail.com>
2022-07-12 17:39:51 +0200

    bonus aggiunto

--- src/main/java/jana60/controller/MainController.java
+++ src/main/java/jana60/controller/MainController.java
@@ -1,6 +1,10 @@
 package jana60.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,4 +19,11 @@ public class MainController {
 		return "home";
 	}
 
+	@GetMapping("/ora")
+	public String orario (Model model) {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		model.addAttribute("orario", now.format(formatter));
+		return "ora";
+	}
 }
